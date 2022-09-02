321ceca54b24775d515368b5ced4fda7df289b87
David Hevesi <davidhevesi2008@gmail.com>
2022-09-02 14:12:57 -0400

    add custom colors and fonts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import styles from "../styles/Home.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <h1 className="text-lg font-SourceSansPro font-thin">Hello world!</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      redish: "#EE2D1C",
+      blackish: "#4c4c51",
+      blueish: "#88939E",
+      white: "#ffff",
+      black: "#0000",
+      // Gradient|
+      sundown: "ffb1b5",
+      "ruby-red": "#EE2E1D",
+    },
+    fontSize: {
+      lg: ["36px", { lineHeight: "36px" }],
+      sm: ["22px", { lineHeight: "22px" }],
+    },
+    fontFamily: {
+      SourceSansPro: "SourceSansPro, sans-serif",
+    },
     extend: {},
   },
   plugins: [],
