85745696087ba57ff132f4ccfe328a9dc97a4f1c
David Hevesi <davidhevesi2008@gmail.com>
2022-09-05 17:46:07 -0400

    refactor main page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-import HearoInfo from '../components/HearoInfo'
-import Hero from '../components/Hero'
-import NavBar from '../components/NavBar'
-
+import HeroSection from '../components/HeroSection'
+import VideoSection from '../components/VideoSection'
+import ExperienceSection from '../components/ExperienceSection'
 export default function Home() {
   return (
-    <div className="relative">
-      <Hero />
-      <NavBar />
-      <HearoInfo />
-    </div>
+    <>
+      <HeroSection />
+      <VideoSection />
+      <ExperienceSection />
+    </>
   )
 }
