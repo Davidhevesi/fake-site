00ebcdffad8348d83db86ad054736e4379382168
David Hevesi <davidhevesi2008@gmail.com>
2022-09-05 17:43:40 -0400

    add navigation and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navigation />
+      <Component {...pageProps} />;
+      <Footer />
+    </>
+  );
 }
 
 export default MyApp;
