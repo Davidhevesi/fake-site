f7e44d303d9f42d97099bfb2261cfe2d2c003283
David Hevesi <davidhevesi2008@gmail.com>
2022-09-05 17:36:07 -0400

    created the experience section

---
+++ components/ExperienceSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function ExperienceSection() {
+  return (
+    <>
+      <section className="px-4 py-4 md:px-0 md:py-0">
+        <div className="container mx-auto pt-10 flex flex-col items-center max-w-2xl space-y-2">
+          <h4 className="text-lg font-SourceSansPro mb-4 md:mb-0">
+            Ready to have your cake and eat it too?
+          </h4>
+          <p className="text-center text-blueish font-SourceSansPro text-sm leading-5">
+            Start by designing the experience you have in mind. We'll guide you
+            through each step. If your experience meets the quality standards,
+            you'll hear more about what's next.
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
